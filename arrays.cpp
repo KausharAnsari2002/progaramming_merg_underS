1c046788aba831b96d87ec49bd738f3362b6c991
kaushar Ansari <kausharansari9373@gmail.com>
2026-09-18 03:24:04 +0530

    new data is added

---
+++ arrays.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+    void display(int arr[], int n){
+        for(int i = 0; i<n; i++){
+            cout<<arr[i]<<" ";
+        }
+    }
+
+int main () {
+
+    int arr[] = {1,2,3,4,5,6};
+    int n  = sizeof(arr)/sizeof(arr[0]);
+
+    display(arr,n);
+
+
+
+
+
+
+
+ return 0;
+
+
+}
